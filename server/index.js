316abcca123bbcc5d391b71363821cf89215efb8
Nelson <nelson.vico@gmail.com>
2023-11-15 11:16:32 +0100

    modif middleware

--- server/index.js
+++ server/index.js
@@ -20,10 +20,10 @@ app
 
 connectDB();
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://clone-youtube-liard.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.append('Access-Control-Allow-Origin', 'https://clone-youtube-liard.vercel.app');
+    res.append('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+    res.append('Access-Control-Allow-Credentials', true);
+    res.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
