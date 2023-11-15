35a76e34a4fc0299c635fbf6e4695c709e415dd7
Nelson <nelson.vico@gmail.com>
2023-11-15 16:43:51 +0100

    test Node

--- server/src/routes/signUp.js
+++ server/src/routes/signUp.js
@@ -13,63 +13,63 @@ module.exports = (app) => {
         const {username, email, password, confirmpassword, image, AuthByGoogle} = req.body;
         const picture_profil = image ? image : '';
      try{
-        if(AuthByGoogle){
-            const existingUser = await Users.findOne({email});
-            if(existingUser){
-               return res.status(400).json({
-                    reason: "ExistingUser",
-                    message: `L\'utilisateur ${email} est déjà existant.`
-                })
+        // if(AuthByGoogle){
+        //     const existingUser = await Users.findOne({email});
+        //     if(existingUser){
+        //        return res.status(400).json({
+        //             reason: "ExistingUser",
+        //             message: `L\'utilisateur ${email} est déjà existant.`
+        //         })
                 
-            }
-            const existingName = await Users.findOne({username});
-            if(existingName){
-               return res.status(400).json({
-                   reason: "ExistingName",
-                   message: `Le nom d'utilisateur ${username} est déjà pris.`
-               })
-            }
-            const hashedPassword = await bycrpt.hash(password, 12);
-            const result = await Users.create({username, email, password: hashedPassword, color: color, image: picture_profil});
+        //     }
+        //     const existingName = await Users.findOne({username});
+        //     if(existingName){
+        //        return res.status(400).json({
+        //            reason: "ExistingName",
+        //            message: `Le nom d'utilisateur ${username} est déjà pris.`
+        //        })
+        //     }
+        //     const hashedPassword = await bycrpt.hash(password, 12);
+        //     const result = await Users.create({username, email, password: hashedPassword, color: color, image: picture_profil});
 
-            const token = jwt.sign(
-            {
-                email: result.email, 
-                username: result.username, 
-                id: result._id,
-                color: result.color,
-                image: result.image
-            },
-                process.env.KEY_JWT_TOKEN,
-                {expiresIn: '1h'}
-                )
-           return res.json({result: 'User valider', token});
-        }else{
+        //     const token = jwt.sign(
+        //     {
+        //         email: result.email, 
+        //         username: result.username, 
+        //         id: result._id,
+        //         color: result.color,
+        //         image: result.image
+        //     },
+        //         process.env.KEY_JWT_TOKEN,
+        //         {expiresIn: '1h'}
+        //         )
+        //    return res.json({result: 'User valider', token});
+        // }else{
 
-        if(!RegexUsername.test(username)){
-          return res.status(401).json({
-                reason: 'username' , 
-                message: 'le nom d\'utilisateur doit contenir au minimum 5 caratères contenant exclusivement des chiffres et lettres.'
-        })
-        }
-        if(!RegexEmail.test(email)){
-           return res.status(401).json({
-                reason: 'email non valide',
-                message: 'Veuillez saisir une adresse email valide' 
-        })
-        }   
-        if(!RegexPassword.test(password)){
-           return res.status(401).json({
-                reason: 'password',
-                message: "Le mot de passe doit contenir au minimum 8 caratères dont 1 lettre miniscule, 1 lettre majscule et un chiffre."
-            })
-        }    
-        if(password !== confirmpassword){
-           return res.status(401).json({
-                reason: 'confirmpassword', 
-                message: 'La confirmation du mot de passe ne correspond pas au mot de passe'
-            });
-        }
+        // if(!RegexUsername.test(username)){
+        //   return res.status(401).json({
+        //         reason: 'username' , 
+        //         message: 'le nom d\'utilisateur doit contenir au minimum 5 caratères contenant exclusivement des chiffres et lettres.'
+        // })
+        // }
+        // if(!RegexEmail.test(email)){
+        //    return res.status(401).json({
+        //         reason: 'email non valide',
+        //         message: 'Veuillez saisir une adresse email valide' 
+        // })
+        // }   
+        // if(!RegexPassword.test(password)){
+        //    return res.status(401).json({
+        //         reason: 'password',
+        //         message: "Le mot de passe doit contenir au minimum 8 caratères dont 1 lettre miniscule, 1 lettre majscule et un chiffre."
+        //     })
+        // }    
+        // if(password !== confirmpassword){
+        //    return res.status(401).json({
+        //         reason: 'confirmpassword', 
+        //         message: 'La confirmation du mot de passe ne correspond pas au mot de passe'
+        //     });
+        // }
         const existingName = await Users.findOne({username});
             if(existingName){
               return res.status(400).json({
@@ -99,7 +99,7 @@ module.exports = (app) => {
             {expiresIn: '12h'}
             )
          return res.json({result: 'User valider', token}) 
-        }// cas google aprés 
+        // cas google aprés 
      }catch(error){
          res.status(500).json({message: "Error server"})   
      }
