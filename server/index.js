d5e9e2fc4435bf8d03bc28e47e2431247c6902c5
Nelson <nelson.vico@gmail.com>
2023-11-15 15:34:05 +0100

    server

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app
 .use(cors({
-    origin: "https://fake-server-youtube-api.vercel.app/sign-up",
+    origin: "https://clone-youtube-liard.vercel.app",
     methods: ['GET','POST','DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
