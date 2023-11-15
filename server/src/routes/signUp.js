c75bf7fd5bb426dcc81c18e7eac38e2a6db62279
Nelson <nelson.vico@gmail.com>
2023-11-15 15:52:09 +0100

    modif signUp

--- server/src/routes/signUp.js
+++ server/src/routes/signUp.js
@@ -16,19 +16,18 @@ module.exports = (app) => {
         if(AuthByGoogle){
             const existingUser = await Users.findOne({email});
             if(existingUser){
-                 res.status(400).json({
+               return res.status(400).json({
                     reason: "ExistingUser",
                     message: `L\'utilisateur ${email} est déjà existant.`
                 })
-               return; 
+                
             }
             const existingName = await Users.findOne({username});
             if(existingName){
-                res.status(400).json({
+               return res.status(400).json({
                    reason: "ExistingName",
                    message: `Le nom d'utilisateur ${username} est déjà pris.`
                })
-              return;
             }
             const hashedPassword = await bycrpt.hash(password, 12);
             const result = await Users.create({username, email, password: hashedPassword, color: color, image: picture_profil});
@@ -44,53 +43,46 @@ module.exports = (app) => {
                 process.env.KEY_JWT_TOKEN,
                 {expiresIn: '1h'}
                 )
-            res.json({result: 'User valider', token});
-            return; 
+           return res.json({result: 'User valider', token});
         }else{
 
         if(!RegexUsername.test(username)){
-            res.status(401).json({
+          return res.status(401).json({
                 reason: 'username' , 
                 message: 'le nom d\'utilisateur doit contenir au minimum 5 caratères contenant exclusivement des chiffres et lettres.'
         })
-            return;
         }
         if(!RegexEmail.test(email)){
-            res.status(401).json({
+           return res.status(401).json({
                 reason: 'email non valide',
                 message: 'Veuillez saisir une adresse email valide' 
         })
-            return;
         }   
         if(!RegexPassword.test(password)){
-            res.status(401).json({
+           return res.status(401).json({
                 reason: 'password',
                 message: "Le mot de passe doit contenir au minimum 8 caratères dont 1 lettre miniscule, 1 lettre majscule et un chiffre."
             })
-            return;
         }    
         if(password !== confirmpassword){
-            res.status(401).json({
+           return res.status(401).json({
                 reason: 'confirmpassword', 
                 message: 'La confirmation du mot de passe ne correspond pas au mot de passe'
             });
-            return;
         }
         const existingName = await Users.findOne({username});
             if(existingName){
-                res.status(400).json({
+              return res.status(400).json({
                    reason: "existingName",
                    message: `Le nom d'utilisateur ${username} est déjà pris.`
                })
-              return;
         }
         const existingUser = await Users.findOne({email});
         if(existingUser){
-             res.status(400).json({
+            return res.status(400).json({
                 reason: "existingUser",
                 message: `L\'utilisateur ${email} est déjà existant.`
             })
-           return; 
         }
         const hashedPassword = await bycrpt.hash(password, 12);
         const result = await Users.create({username, email, password: hashedPassword, color: color, image: picture_profil});
@@ -106,8 +98,8 @@ module.exports = (app) => {
             process.env.KEY_JWT_TOKEN,
             {expiresIn: '12h'}
             )
-         res.json({result: 'User valider', token})   
-        }// cas google aprés  
+         return res.json({result: 'User valider', token}) 
+        }// cas google aprés 
      }catch(error){
          res.status(500).json({message: "Error server"})   
      }
